2064b719a98abc87aa9ddd9ac83bdedfe5d1357f
Eunbi <peb7707@naver.com>
2025-05-30 17:22:11 +0900

    25_05_30
    spring 설정한거 다시 한번 복습한 것.

--- Spring/Mi/src/main/java/com/peisia/controller/TestController.java
+++ Spring/Mi/src/main/java/com/peisia/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.peisia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,28 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.peisia.service.TestService;
 
-import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @RequestMapping("/test/*")
-@AllArgsConstructor
+//@AllArgsConstructor
 @Controller
 public class TestController {
+		
+	@Setter(onMethod_ = @Autowired)
 	private TestService service;
 	
 	@GetMapping("/getOnePlusTwo")
-//	public void getOnePlusTwo(Model model) {
-	public void getOnePlusTwo() {
+	public void getOnePlusTwo(Model model) {
+//	public void getOnePlusTwo() {
 		log.info("getOnePlusTwo ==========================================");
 		String one = service.getOne();
 		String two = service.getTwo();
 		Integer sum = Integer.parseInt(one) + Integer.parseInt(two); 
 		
-		System.out.println("sdjks");
-		
 		log.info("(여기 컨트롤러임) 1 더하기 2는 = " + sum);
 		
-		//model.addAttribute("sum",sum);
+		model.addAttribute("sum",sum);
 	}
 }
